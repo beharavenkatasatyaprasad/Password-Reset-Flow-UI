17547d1f80addad976c3b0d9c7d3317a91a7f1fc
beharavenkatasatyaprasad <satyaprasadbehara@gmail.com>
2020-11-29 20:18:19 +0530

    commit

--- js/home.js
+++ js/home.js
@@ -1,48 +1,13 @@
 checklogin();
 
-// function check() {
-//     const token = getCookie('jwt');
-//     if (!token) {
-//         custom_alert("warning", "UnAuthorized Login!!!");
-//         document.cookie = 'jwt' + '=; Max-Age=0';
-//         window.localStorage.removeItem('user')
-//         setTimeout(() => {
-//             window.location.href = "./index.html";
-//         }, 3000);
-//     } else {
-//         checklogin(token);
-//     }
-// }
-
-
-// function getCookie(cname) {
-//     var name = cname + "=";
-//     var decodedCookie = decodeURIComponent(document.cookie);
-//     var ca = decodedCookie.split(';');
-//     for (var i = 0; i < ca.length; i++) {
-//         var c = ca[i];
-//         while (c.charAt(0) == ' ') {
-//             c = c.substring(1);
-//         }
-//         if (c.indexOf(name) == 0) {
-//             return c.substring(name.length, c.length);
-//         }
-//     }
-//     return "";
-// }
-
 async function checklogin() {
-    // let data = {
-    //     token: token
-    // }
-    let datares = await fetch('https://password-reset-flow-server.herokuapp.com/checklogin', {
+    let response = await fetch('https://password-reset-flow-server.herokuapp.com/checklogin', {
         method: 'GET',
-        // body: JSON.stringify(data),
         headers: {
             'Content-Type': 'application/json'
         }
     });
-    const res = await datares.json()
+    const res = await response.json()
     if (res.type_ == 'success') {
         document.getElementById('userwelcome').innerHTML=(`
         <div class="col-lg-6 fade-in col-sm-12 create-fields" style="margin: auto; margin-top: 7%;">
@@ -59,11 +24,16 @@ async function checklogin() {
 }
 
 
-// function logout() {
-//     custom_alert("success", "Logging Out!!!");
-//     document.cookie = 'jwt' + '=; Max-Age=0';
-//     window.localStorage.removeItem('user')
-//     setTimeout(() => {
-//         window.location.href = "./index.html"
-//     }, 3000);
-// }
+async function logout() {
+    let response = await fetch('https://password-reset-flow-server.herokuapp.com/logout', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const res = await response.json()
+    custom_alert(res.type_,res.message);
+    setTimeout(() => {
+        window.location.href = "./index.html"
+    }, 3000);
+}
