3cb3bf66f3540a8cf17ea483c6ac47ef65b09ae3
beharavenkatasatyaprasad <satyaprasadbehara@gmail.com>
2020-11-29 21:12:01 +0530

    commit

--- js/home.js
+++ js/home.js
@@ -8,18 +8,18 @@ async function checklogin() {
         }
     });
     const res = await response.json()
-    if (res.type_ == 'success') {
+    custom_alert(res.type_ , res.message);
+    if (res.type_ != 'success') {
+        setTimeout(() => {
+            window.location.href = "./index.html"
+        }, 3000);
+    } else {
         document.getElementById('userwelcome').innerHTML=(`
         <div class="col-lg-6 fade-in col-sm-12 create-fields" style="margin: auto; margin-top: 7%;">
             <h1 id="username" class="text-center">Welcome ${res.user.split('@')[0]}</h1>
         </div>
         `)
         window.localStorage.setItem('user', res.user);
-    } else {
-        custom_alert('danger', 'unauthorized login !!!');
-        setTimeout(() => {
-            window.location.href = "./index.html"
-        }, 3000);
     }
 }
 
